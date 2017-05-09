2c5ef776ccae9383d7040a3b643b3f4e050b2514
jmrigali <jmrigali@ymail.com>
2017-05-09 11:09:23 -0600

    identified values for table, still need to append them to table

--- js/nutrients.js
+++ js/nutrients.js
@@ -10,35 +10,44 @@
 
 
 
-
   function totalNutrients(array) {
     let total = 0;
     let $total = $('#totalNutrients').children('tbody');
+    // var $identifier = $($total).children('tr').children('td').next();
+    // console.log($($identifier).attr('id'));
 
     array.forEach(function(element, index) {
+      var $cell = $($total).children('tr').children('#'+`${index}`)[0];
+      var identify = $($cell).attr('id');
+      console.log(index);
+      console.log(identify);
 
-      switch (index) {
-        case index === 1:
-        $($total).children($('tr').children($('#1').text(total)));
-
-        case index === 2:
-        $($total).children($('tr').children($('#2').text(total)));
-
-        case index === 3:
-        $($total).children($('tr').children($('#3').text(total)));
-
-        case index === 4:
-        $($total).children($('tr').children($('#4').text(total)));
-
-        case index === 5:
-        $($total).children($('tr').children($('#5').text(total)));
-
-        case index === 6:
-        $($total).children($('tr').children($('#6').text(total)));
-
-        default:
+      if(index == identify) {
+        var value = parseInt(element.facts);
         
       }
+
+      // switch (index) {
+      //   case index === 1:
+      //   $($total).children($('tr').children($('#1').html(total)));
+      //   case index === 2:
+      //   $($total).children($('tr').children($('#2').html(total)));
+      //
+      //   case index === 3:
+      //   $($total).children($('tr').children($('#3').html(total)));
+      //
+      //   case index === 4:
+      //   $($total).children($('tr').children($('#4').html(total)));
+      //
+      //   case index === 5:
+      //   $($total).children($('tr').children($('#5').html(total)));
+      //
+      //   case index === 6:
+      //   $($total).children($('tr').children($('#6').html(total)));
+      //
+      //   default:
+      //
+      // }
     });
   }
   totalNutrients(storageArr);
