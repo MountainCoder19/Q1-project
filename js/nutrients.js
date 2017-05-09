a36a81cc9743e0d09f2dc98117791b4dc7fe1eea
jmrigali <jmrigali@ymail.com>
2017-05-09 11:52:33 -0600

    changed a few values in nutrientReport function

--- js/nutrients.js
+++ js/nutrients.js
@@ -1,6 +1,7 @@
 
   var aggArr = "";
   var storageArr = JSON.parse(localStorage.getItem("aggArr"));
+  var aggNutrients =
 
   function refresh() {
     localStorage.setItem("aggArr", JSON.stringify(nutrientArr));
@@ -17,38 +18,16 @@
     // console.log($($identifier).attr('id'));
 
     array.forEach(function(element, index) {
-      let total = 0;
-      var $cell = $($total).children('tr').children('#'+`${index}`)[0];
-      var identify = $($cell).attr('id');
+
+      let $cell = $($total).children('tr').children('#'+`${index}`)[0];
+      let identify = $($cell).attr('id');
 
       if(index == identify) {
-        total += parseInt(element.facts);
+        let value = parseInt(element.facts);
         var unit = $($cell).html();
-        $($cell).html(total + unit);
-
+        $($cell).html(value + unit);
       }
-
-      // switch (index) {
-      //   case index === 1:
-      //   $($total).children($('tr').children($('#1').html(total)));
-      //   case index === 2:
-      //   $($total).children($('tr').children($('#2').html(total)));
-      //
-      //   case index === 3:
-      //   $($total).children($('tr').children($('#3').html(total)));
-      //
-      //   case index === 4:
-      //   $($total).children($('tr').children($('#4').html(total)));
-      //
-      //   case index === 5:
-      //   $($total).children($('tr').children($('#5').html(total)));
-      //
-      //   case index === 6:
-      //   $($total).children($('tr').children($('#6').html(total)));
-      //
-      //   default:
-      //
-      // }
     });
+
   }
   totalNutrients(storageArr);
