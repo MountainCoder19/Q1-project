cb20595c6da0c8eb4958a9e23255bd6fc0235f77
jmrigali <jmrigali@ymail.com>
2017-05-08 22:37:20 -0600

    nutrient table does not append totals yet

--- js/nutrients.js
+++ js/nutrients.js
@@ -1,43 +1,44 @@
-var aggArr = "";
 
-
-function refresh() {
-  localStorage.setItem("aggArr", JSON.stringify(nutrientArr));
-  window.location.reload();
+  var aggArr = "";
   var storageArr = JSON.parse(localStorage.getItem("aggArr"));
-  console.log(storageArr);
+
+  function refresh() {
+    localStorage.setItem("aggArr", JSON.stringify(nutrientArr));
+    window.location.reload();
+
+  }
+
+
+
+
+  function totalNutrients(array) {
+    let total = 0;
+    let $total = $('#totalNutrients').children('tbody');
+
+    array.forEach(function(element, index) {
+
+      switch (index) {
+        case index === 1:
+        $($total).children($('tr').children($('#1').text(total)));
+
+        case index === 2:
+        $($total).children($('tr').children($('#2').text(total)));
+
+        case index === 3:
+        $($total).children($('tr').children($('#3').text(total)));
+
+        case index === 4:
+        $($total).children($('tr').children($('#4').text(total)));
+
+        case index === 5:
+        $($total).children($('tr').children($('#5').text(total)));
+
+        case index === 6:
+        $($total).children($('tr').children($('#6').text(total)));
+
+        default:
+        
+      }
+    });
+  }
   totalNutrients(storageArr);
-}
-
-
-function totalNutrients(array) {
-  let total = 0;
-  let $table = $("#total-nutrients");
-  console.log($table);
-
-  array.forEach(function(element) {
-    switch (element) {
-      case index === 1:
-
-      console.log(element);
-        break;
-      case index === 2:
-      console.log(element);
-        break;
-      case index === 3:
-      console.log(element);
-        break;
-      case index === 4:
-      console.log(element);
-        break;
-      case index === 5:
-      console.log(element);
-        break;
-      case index === 6:
-      console.log(element);
-        break;
-      default:
-
-    }
-  })
-}
